1d793197090870e05bc22c0df43fcd2203e1991f
balint <b>
2021-08-25 18:09:45 +0200

    es torlese

--- feladatgy_I_1/feladatgy_I_1.cpp
+++ feladatgy_I_1/feladatgy_I_1.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     int szam = 0;
-    printf("Adj meg egy egesz szamot es megmondom, paros-e: ");
+    printf("Adj meg egy egesz szamot megmondom, paros-e: ");
     scanf_s("%d", &szam);
     if (szam % 2 == 0) {
         std::cout << "A szam paros.\n";
