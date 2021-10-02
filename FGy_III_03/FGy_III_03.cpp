bf1b9b338ceea567618ba2579e4c0a3273cf8940
horbal00 <balintus98@gmail.com>
2021-10-02 16:28:28 +0200

    Az FGy. III. 3.feladat kész.

---
+++ FGy_III_03/FGy_III_03.cpp
@@ -0,0 +1,43 @@
+// FGy_III_03.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int valodi_osztok_szama(int szam)
+{
+    int seged = 0;
+    for (int i = 2; i < szam; i++)
+    {
+        if (szam % i == 0)
+        {
+            seged += 1;
+        }
+    }
+    return seged;
+}
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    cout << valodi_osztok_szama(10);
+    cout << "\n";
+    cout << valodi_osztok_szama(11);
+    cout << "\n";
+    cout << valodi_osztok_szama(100);
+    cout << "\n";
+    cout << valodi_osztok_szama(1000);
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
