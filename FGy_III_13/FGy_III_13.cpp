e459e16ecfb7cbdfa14227e29cd30d290c05e08b
horbal00 <balintus98@gmail.com>
2021-10-14 18:30:39 +0200

    Az FGy. III. 13.feladat kész.

---
+++ FGy_III_13/FGy_III_13.cpp
@@ -0,0 +1,47 @@
+// FGy_III_13.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <list>
+#include <cmath>
+
+using namespace std;
+
+double negyzetgyok_osszeg()
+{
+    string szam;
+    list<int> szamsor;
+    double osszeg = 0;
+    double negyzetgyok = 0;
+    do
+    {
+        getline(cin, szam);            
+        if (szam != "")
+        {
+            szamsor.push_back(stoi(szam));
+        }
+    } while (!szam.empty());
+    for (list<int>::iterator it = szamsor.begin(); it != szamsor.end(); it++)
+    {
+        negyzetgyok = sqrt(*it);
+        osszeg += negyzetgyok;
+    }
+    return osszeg;
+}
+int main()
+{
+    std::cout << "Hello World!\n";
+    cout << negyzetgyok_osszeg();
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
