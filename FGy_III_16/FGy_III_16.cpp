5398ffee954e7c989bbb88a5dadeed3ddc42779e
horbal00 <balintus98@gmail.com>
2021-10-22 15:06:25 +0200

    Az FGy. III. 16.feladatnal a szorast nem tudom hogyan kene csinalni.

---
+++ FGy_III_16/FGy_III_16.cpp
@@ -0,0 +1,71 @@
+// FGy_III_16.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <list>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+bool egesz_szam_e(string szam)
+{
+    bool egesz = true;
+    for (int i = 0; i < szam.size(); i++)
+    {
+        if (szam[i] < '0' || szam[i] > '9')
+        {
+            egesz = false;
+        }
+    }
+    return egesz;
+}
+
+double atlagos_kulonbseg()
+{
+    string szam;
+    list<int> szamsor;
+    double atlag = 0;
+    double osszeg = 0;
+    double szamlalo = 0;
+    double szoras = 0;
+    getline(cin, szam);
+    if (szam != "" && egesz_szam_e(szam) == true)
+    {
+        szamsor.push_back(stoi(szam));
+    }
+    while (!szam.empty())
+    {
+        getline(cin, szam);
+        if (szam != "" && egesz_szam_e(szam) == true)
+        {
+            szamsor.push_back(stoi(szam));
+        }
+    }
+    for (list<int>::iterator it = szamsor.begin(); it != szamsor.end(); it++)
+    {
+        for (int i = 1; i < szamsor.size(); i++)
+        {
+            osszeg += *it;
+            atlag = osszeg / i;
+            szoras = sqrt(pow(atlag - *it, 2) / i);
+        }
+    }
+    return szoras;
+}
+int main()
+{
+    std::cout << "Hello World!\n";
+    cout << atlagos_kulonbseg();
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
